b0fcd56d229a3e6086f444c76ffa9249d06f5768
royquillca <ccus.andy.quillca@gmail.com>
2023-03-03 18:35:36 -0500

    Presentation file page customized

--- pages/1_👩‍💻_Presentacion.py
+++ pages/1_👩‍💻_Presentacion.py
@@ -7,10 +7,20 @@ import streamlit as st
 from utils.kpis import calc_volatility, calc_agr, calculate_cumulative_return
 # from utils.
 
-st.subheader('**Análisis de inversión en el mercado bursátil**')
-# st.markdown('**Descripción del problema**')
-# st.markdown('**Objetivos**')
+st.markdown('<h4 align="center">Análisis de inversión en el mercado bursátil - Proyecto Individual 3 de Data Analytics del Bootcamp Data Science en Soy Henry</h4>', unsafe_allow_html=True)
+st.markdown('**Descripción del problema**')
 
+st.markdown("En este proyecto simulamos una situación en donde una empresa busca invertir en el mercado bursátil y nos solicita analizarlo en detalle. Como expertos en datos, estamos en capacidad de brindar una explicación de lo que ha sucedido en este mercado en los últimos 23 años (considerando impactos positivos y negativos a partir del año 2000), recomendaciones de inversión y otra información complementaria. El foco del análisis es variado y amplio, pero nos limitamos a las empresas pertenecientes al índice SP500 (Standard & Poor's 500 Index).")
+
+st.markdown('**Objetivos**')
+
+st.markdown(":male-detective::mag_right::bar_chart: Desarrollar un Análisis Exploratorio de los datos (EDA) con el fin de comprender mejor los datos y encontrar patrones, outliers y/o anomalías.")
+
+st.markdown(":computer::male-technologist::rocket: Desarrollar un Dashboard funcional usando [streamlit](https://streamlit.io/) y coherente con el análisis y la historia que queríamos relatar")
+
+st.markdown(":chart_with_upwards_trend::chart_with_downwards_trend::handshake: Elaborar gráficos interactivos que nos permiten ver la variación de precios en el tiempo, comparar entre distintas acciones, cálculo de estadísticas bursátiles y recomendaciones basadas en el retorno y riesgo de inversión.")
+
+st.markdown(":bar_chart::chart_with_upwards_trend::busts_in_silhouette: Proponer 3 KPIs en el desarrollo del Dashboard y deben estar relacionados con la historia que se va a contar.")
 
 
 option = st.selectbox('Selecciona la compañía', ('MTD','AVGO','NVR'))
@@ -36,14 +46,16 @@ st.image('assets/treplot_sp500.png')
 
 
 # Información personal
-st.markdown("""<p align="center"><b>Sígueme en mis redes sociales en:</b></p>""",unsafe_allow_html=True)
+st.markdown("""<p align="left"><b>Acerca del proyecto:</b></p>""",unsafe_allow_html=True)
 st.markdown("""
 <style>
-    .social-media {
-        display:flex;
-        justify-content: space-between;
-        # justify-content: center;
-        # justify-content: space-around;
+    p {
+        padding-top: 0;
+        margin: 0;
+    }
+    p < b {
+        padding-bottom: 0;
+        margin: 0;
     }
     a {
         text-decoration: none;
@@ -59,25 +71,6 @@ st.markdown("""
     .icon:hover {
     transform: scale(1.5);
     }
-    
-</style>
-<div class="social-media">
-    <a href="https://github.com/royquillca" target="blank">
-        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" alt="royquillca" height="15" width="20" class="icon"> royquillca
-    </a>
-    <a href="https://linkedin.com/in/royquillca" target="blank">
-        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="royquillca" height="15" width="20" class="icon"> royquillca
-    </a>
-    <a href="https://www.instagram.com/royquillca/" target="blank">
-        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="@royquillca" height="15" width="20" class="icon"> @royquillca
-    </a>
-    <a href="https://www.youtube.com/@royquillca" target="blank">
-        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg" alt="@roquidev" height="15" width="20" class="icon"> @royquillca
-    </a>
-    <a href="https://twitter.com/royquillca" target="blank" >
-        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="royquillca" height="15" width="20" class="icon"> @royquillca
-    </a>
-</div>
 """, unsafe_allow_html=True)
 
-st.write('<p align="center">Puedes revisar mis proyectos personales accediendo al link: <a href="https://royquillca.github.io/portafolio/">Portafolio</a></p>', unsafe_allow_html=True)
+st.write('<p align="center">Puedes revisar el repositorio de este proyecto <a href="https://github.com/royquillca/pida_henry">Proyecto Individual de Data Analytics (PIDA)</a>. Así mismo, te invito a que revises mis proyectos personales accediendo a mi <a href="https://royquillca.github.io/portafolio/">Portafolio</a></p>', unsafe_allow_html=True)
