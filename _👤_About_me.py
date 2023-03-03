3c13e970a892f518be00f67e131e1c1173ba6e53
royquillca <ccus.andy.quillca@gmail.com>
2023-03-03 18:38:10 -0500

    About page customized

---
+++ _👤_About_me.py
@@ -0,0 +1,67 @@
+import streamlit as st
+
+st.markdown('<h1 align="center">Análisis de inversión en el mercado bursátil elaborado por <i>Roy Quillca</i></h1>', unsafe_allow_html=True)
+
+st.markdown(
+"""
+<p align='justify'>
+¡Hola! Soy Roy Quillca, un Data Scientist apasionado por el análisis de datos y la creación de visualizaciones interactivas para contar historias a partir de ellos. Tengo experiencia trabajando en proyectos de ciencia de datos y análisis estadístico en diferentes industrias, y me encanta utilizar herramientas como Python, SQL y tecnología de BIg Data para extraer información valiosa de los datos y presentarla de manera clara y concisa.
+
+En este proyecto de dashboard desarrollado con Streamlit, me he encargado de desarrollar el ETL de inicio a fin usando la librería yfinance para luego desarrolllar las visualizaciones interactivas que permitan a los usuarios explorar y comprender mejor los datos, y he utilizado mis habilidades de análisis de datos para identificar patrones, outliers y anomalías que ayuden a contar una historia convincente sobre la inversión bursátil de las empresas que son parte del Indice S&P 500.
+
+Siempre estoy buscando formas de mejorar y aprender cosas nuevas, y disfruto manipulando y analizando datos para ayudar a las organizaciones o empresas tomar mejores decisiones. Si tienes alguna pregunta sobre mi trabajo en este proyecto o quieres discutir oportunidades de colaboración, no dudes en contactarme.
+</p>""", unsafe_allow_html=True
+)
+
+st.markdown("<p align='center'>¡Gracias por visitar mi perfil!</p>", unsafe_allow_html=True
+)
+
+
+
+
+# Información personal
+st.markdown("""<p align="center"><b>Sígueme en mis redes sociales en:</b></p>""",unsafe_allow_html=True)
+st.markdown("""
+<style>
+    .social-media {
+        display:flex;
+        justify-content: space-between;
+        # justify-content: center;
+        # justify-content: space-around;
+    }
+    a {
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: none;
+    }
+    
+    .icon {
+    transition: transform 0.5s;
+    }
+
+    .icon:hover {
+    transform: scale(1.5);
+    }
+    
+</style>
+<div class="social-media">
+    <a href="https://github.com/royquillca" target="blank">
+        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg" alt="royquillca" height="35" width="40" class="icon"> royquillca
+    </a>
+    <a href="https://linkedin.com/in/royquillca" target="blank">
+        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="royquillca" height="35" width="40" class="icon"> royquillca
+    </a>
+    <a href="https://www.instagram.com/royquillca/" target="blank">
+        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="@royquillca" height="35" width="40" class="icon"> @royquillca
+    </a>
+    <a href="https://www.youtube.com/@royquillca" target="blank">
+        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg" alt="@roquidev" height="35" width="40" class="icon"> @royquillca
+    </a>
+    <a href="https://twitter.com/royquillca" target="blank" >
+        <img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="royquillca" height="35" width="40" class="icon"> @royquillca
+    </a>
+</div>
+""", unsafe_allow_html=True)
+
+st.write('<p align="center">Puedes revisar mis proyectos personales accediendo al link: <a href="https://royquillca.github.io/portafolio/">Portafolio</a></p>', unsafe_allow_html=True)
